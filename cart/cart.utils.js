354adf1a7d7c10cdf1a10a108b1e951dbbb383d1
KB <katrinkajb@gmail.com>
2021-01-21 16:23:41 -0800

    fixing a delete

--- cart/cart.utils.js
+++ cart/cart.utils.js
@@ -1,3 +1,6 @@
+const CART = 'CART';
+const defaultEmptyCart = [];
+
 export function findById(id, array) {
     for (let item of array) {
         if (item.id === id) {
@@ -38,5 +41,46 @@ export function calcCartTotal(cart, cocktails) {
         cartTotal = cartTotal + cocktailTotal;
     }
     return cartTotal;
-
 }
+
+export function getCart() {
+    const stringCart = localStorage.getItem(CART);
+    if (stringCart) {
+        const parsedCart = JSON.parse(stringCart);
+        return parsedCart;
+    } else {
+        const stringDefaultCart = JSON.stringify(defaultEmptyCart);
+        localStorage.setItem(CART, stringDefaultCart);
+
+        return defaultEmptyCart;
+    }
+}
+
+export function clearCart() {
+    const stringyDefaultCart = JSON.stringify(defaultEmptyCart);
+
+    localStorage.setItem(CART, stringyDefaultCart);
+}
+
+
+export function setCart(cart) {
+    const stringyCart = JSON.stringify(cart);
+
+    localStorage.setItem(CART, stringyCart);
+}
+
+
+// addToCart
+// 1. Make it so you can click on products and add them to the cart in localStorage
+
+    // a) We need a button to click
+    // b) Add an event listener to the button
+    // c) On click, we should
+    //      -Look at the id of the thing we clicked on.
+    //      -We should add/increment that item in the cart. 0) Get the cart from localStorage
+//              - Check if an item with this ID is already in the cart.
+                // -If so, increment the quantity
+                // -If it's not in the cart, put one in there with a quantity of 1.
+// 2. Load the cart page from localStorage
+    // a) Grab the cart from localStorage, and store it in a variable.
+    // b) Replace our hard-coded cart with that variable.
